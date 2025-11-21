6f5fb4834b8d617a2ff5164245f880c1fda4bec1
Allen <tkmzor@gmail.com>
2025-11-21 22:33:04 +0200

    debug

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,15 @@
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import React, { useState } from "react";
+import UserContext from "./UserContext";
+import Main from "./Main"; // replace with your root component
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+    const [user, setUser] = useState(null);
 
-  return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            <Main />
+        </UserContext.Provider>
+    );
 }
 
 export default App;
