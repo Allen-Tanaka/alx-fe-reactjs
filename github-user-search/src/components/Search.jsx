73a4936146e2b5dc89d66ceaf6e9a810137180a2
Allen <tkmzor@gmail.com>
2025-11-23 23:36:46 +0200

    github user search

---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,8 @@
+export default function Search() {
+  return (
+    <div>
+      <h2>GitHub User Search</h2>
+      <input type="text" placeholder="Search GitHub username..." />
+    </div>
+  );
+}
