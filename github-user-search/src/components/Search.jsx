2375d14d6f5315e8ad8a27e53547ac8813a1ffdc
Allen <tkmzor@gmail.com>
2025-11-23 23:43:22 +0200

    debug error state

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,7 +20,7 @@ export default function Search() {
       const data = await fetchUserData(query);
       setUser(data);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
