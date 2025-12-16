fc788bb8e6c226162d72028e510690b7f59262ab
Allen <tkmzor@gmail.com>
2025-12-16 23:22:48 +0200

    add useAuth hook

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,12 @@
 import { Navigate } from "react-router-dom";
 
+const useAuth = () => {
+  const isAuthenticated = false; // simulate auth state
+  return { isAuthenticated };
+};
+
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = false; // simulate auth
+  const { isAuthenticated } = useAuth();
 
   if (!isAuthenticated) {
     return <Navigate to="/" replace />;
